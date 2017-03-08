1e0cb8b6c88d47826ab35d7fa05e04564e5f6086
Mark Wubben <mark@novemberborn.net>
2017-03-08 15:14:00 +0000

    Export public API

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 'use strict'
+
+const compare = require('./lib/compare')
+const describe = require('./lib/describe')
+const diff = require('./lib/diff')
+const format = require('./lib/format')
+
+exports.compare = compare.compare
+exports.compareDescriptors = compare.compareDescriptors
+
+exports.describe = describe
+
+exports.diff = diff.diff
+exports.diffDescriptors = diff.diffDescriptors
+
+exports.format = format.format
+exports.formatDescriptor = format.formatDescriptor
