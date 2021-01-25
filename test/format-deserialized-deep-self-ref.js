5cf23a1a73e414a3312743cdde69642137a490f7
ninevra <ninevra@gmail.com>
2021-01-25 01:04:35 -0800

    Clean up the test case

--- test/format-deserialized-deep-self-ref.js
+++ test/format-deserialized-deep-self-ref.js
@@ -1,26 +1,26 @@
 const test = require('ava')
 
-const concordance = require('..')
+const { describe, deserialize, formatDescriptor, serialize } = require('..')
 
 test('formats deserialized self references beyond maxDepth', t => {
   const value = {}
-  const actualDescriptor = concordance.describe({
-    // The nested property's name must be lexically less than the shallow
-    // property's name, so that it's first in the serialization and the shallow
-    // property's value's serialization is a pointer to the nested property's
-    // contents
+  const descriptor = describe({
+    // The nesting property's name must be lexically less than the shallow
+    // property's name, so that it comes first in the serialization and the
+    // shallow property's value's serialization is a pointer to the nesting
+    // property's contents
     a: {
       b: value,
     },
     c: value,
   })
-  const serializedActual = concordance.serialize(actualDescriptor)
-  const deserializedActual = concordance.deserialize(serializedActual)
+  const serialized = serialize(descriptor)
+  const deserialized = deserialize(serialized)
 
   let formatted
 
   t.notThrows(() => {
-    formatted = concordance.formatDescriptor(deserializedActual, { maxDepth: 1 })
+    formatted = formatDescriptor(deserialized, { maxDepth: 1 })
   })
 
   t.snapshot(formatted)
