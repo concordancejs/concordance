c3a8e176ccbe8ca6d396446cd3146e0b91d64ea8
Mark Wubben <mark@novemberborn.net>
2025-05-03 21:47:21 +0200

    Test WeakSetRepresentation

---
+++ src/values/objects/test/weak-set.ts
@@ -0,0 +1,102 @@
+import test from 'ava'
+import { DescriptionContext } from '../../../describe.ts'
+import { Encoder } from '../../../serialize.ts'
+import { Decoder, DeserializationContext } from '../../../deserialize.ts'
+import { WeakSetRepresentation } from '../weak-set.ts'
+import { possiblyEqual, strictlyEqual, unequal } from '../../../comparison.ts'
+import { staticTypeTable } from '../../../serialization-types.ts'
+import { snapshotEncoded } from '../../test/helpers/snapshot-encoded.ts'
+
+// Deserialize method test
+test('deserialize creates a comparable WeakSetRepresentation', (t) => {
+  const originalContext = new DescriptionContext()
+  const weakSet = new WeakSet()
+  const original = originalContext.represent(weakSet) as WeakSetRepresentation
+
+  const encoder = new Encoder()
+  original.serialize(encoder)
+
+  const decoder = new Decoder(encoder.bytes)
+  decoder.staticType() // Consume the type
+  const deserializationContext = new DeserializationContext(decoder)
+  const deserialized = WeakSetRepresentation.deserialize(deserializationContext, decoder)
+
+  // The deserialized representation should be comparable to the original
+  // Since weak sets can't be iterated, the comparison will be possiblyEqual
+  t.is(original.compare(deserialized), possiblyEqual)
+})
+
+// Compare method tests
+test('compare returns strictlyEqual when comparing the same weakSet instance', (t) => {
+  const context = new DescriptionContext()
+  const weakSet = new WeakSet()
+
+  const weakSetRep1 = context.represent(weakSet) as WeakSetRepresentation
+  const weakSetRep2 = context.represent(weakSet) as WeakSetRepresentation
+
+  t.is(weakSetRep1.compare(weakSetRep2), strictlyEqual)
+})
+
+test('compare returns unequal when comparing to non-WeakSetRepresentation', (t) => {
+  const context = new DescriptionContext()
+  const weakSet = new WeakSet()
+  const obj = {}
+
+  const weakSetRep = context.represent(weakSet) as WeakSetRepresentation
+  const objRep = context.represent(obj)
+
+  t.is(weakSetRep.compare(objRep), unequal)
+})
+
+test('compare returns possiblyEqual when comparing different weakSet instances', (t) => {
+  const context = new DescriptionContext()
+  const weakSet1 = new WeakSet()
+  const weakSet2 = new WeakSet()
+
+  // Add the same value to both sets
+  const value = {}
+  weakSet1.add(value)
+  weakSet2.add(value)
+
+  const weakSetRep1 = context.represent(weakSet1) as WeakSetRepresentation
+  const weakSetRep2 = context.represent(weakSet2) as WeakSetRepresentation
+
+  // Since we can't iterate over weak sets to compare their contents,
+  // the comparison result for different instances is possiblyEqual
+  t.is(weakSetRep1.compare(weakSetRep2), possiblyEqual)
+})
+
+test('compare returns unequal when comparing WeakSets with different constructor names', (t) => {
+  const context = new DescriptionContext()
+
+  // Create a regular WeakSet
+  const weakSet = new WeakSet()
+  const weakSetRep = context.represent(weakSet) as WeakSetRepresentation
+
+  // Create a subclass of WeakSet to get a different constructor name
+  class CustomWeakSet extends WeakSet {}
+  const customWeakSet = new CustomWeakSet()
+
+  // This will still be represented as WeakSetRepresentation but with a different constructor name
+  const customWeakSetRep = context.represent(customWeakSet) as WeakSetRepresentation
+
+  // We can now test handling of different constructor names
+  t.is(weakSetRep.compare(customWeakSetRep), unequal)
+})
+
+// Serialization test
+test('serialize uses weakSet static type', (t) => {
+  const context = new DescriptionContext()
+  const weakSet = new WeakSet()
+  const weakSetRep = context.represent(weakSet) as WeakSetRepresentation
+
+  const encoder = new Encoder()
+  weakSetRep.serialize(encoder)
+
+  // Check the overall structure and type with a snapshot
+  snapshotEncoded(t, encoder, 'weakSet serialization')
+
+  // Verify the static type
+  const decoder = new Decoder(encoder.bytes)
+  t.is(decoder.staticType(), staticTypeTable.weakSet)
+})
